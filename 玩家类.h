26bd9fd22be3e045f7e63480ff66c581b3960904
czl411 <60053567+czl411@users.noreply.github.com>
2020-05-06 11:11:55 +0800

    Update 玩家类.h

--- 玩家类.h
+++ 玩家类.h
@@ -1,11 +1,10 @@
  class Player 
  { 
  private: 
-    int fraction;//用来存储得分 
     char key;//用来存储按键 
  public: 
  	void addition(); //对方增加一行 
- 	void eliminateRow(HANDLE hOut, int map[21][12], int &val, int &fraction, int &checkpoint); 
+ 	void eliminateRow(HANDLE hOut, int map[21][12], int &val, int &score, int &checkpoint); 
  	//判断是否能消行并更新分值 
   void level();//难度调整
  }; 
