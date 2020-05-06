3ed29a0b03b30a4e89c07a3b6258018bc2e39b32
czl411 <60053567+czl411@users.noreply.github.com>
2020-05-06 10:45:38 +0800

    Update 方块类.h

--- 方块类.h
+++ 方块类.h
@@ -1,16 +1,16 @@
-1 class Block 
-2 { 
-3 private: 
-4     int positionX[4], positionY[4];//方块的坐标 
-5     bool check;//检测方块是否结束进程  
-6     int val;//方块下落速度 
-7 public: 
-8 void roundBlock();//随机生成方块 
-9 bool collisionDetection();//检测是否碰撞到边框  
-10 void myLeft();//左移 
-11 void myRight();//右移 
-12 void myUp();//顺时针旋转90度 
-13 int myDown();//加速下落 
-14 
- 
-15 }; 
+ class Block 
+ { 
+ private: 
+     int positionX[4], positionY[4];//方块的坐标 
+     bool check;//检测方块是否结束进程  
+     int val;//方块下落速度 
+ public: 
+ void roundBlock();//随机生成方块 
+ bool collisionDetection();//检测是否碰撞到边框  
+ void myLeft();//左移 
+ void myRight();//右移 
+ void myUp();//顺时针旋转90度 
+ int myDown();//加速下落 
+  void myadd();//增行
+  void mycut();//消行
+ }; 
